5375c7296f2329e10b7bcbf32c2670f04ec1b06f
Rishabh <rishabhgupta7199@gmail.com>
2022-12-04 20:10:04 +0530

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -11,5 +11,9 @@ export class PostCreateComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  newPost = '';
+  onAddPost(value:HTMLTextAreaElement) {
+    this.newPost = value.value;
+  }
 
 }
