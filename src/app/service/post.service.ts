3b759d29da29cc36246190189307b59cb2daf97a
Rishabh <rishabhgupta7199@gmail.com>
2022-12-04 23:18:17 +0530

    added post variable and getter for post

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Post } from '../model/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  private posts: Post[] = [];
 
+  
+  public get getPost() {
+    return this.posts;
+  }
+  
   constructor() { }
 }
