b17a5a4bd5bcd28b943ae3cf364ec5e03b7c4e2a
Rishabh <rishabhgupta7199@gmail.com>
2022-12-04 23:39:32 +0530

    used getPostUpdateListner to set posts value and added destroy method to destory

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Post } from 'src/app/model/post';
 import { PostService } from 'src/app/service/post.service';
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
 
   constructor(private postService: PostService) { }
+  ngOnDestroy(): void {
+    if (this.postSub) {
+      this.postSub.unsubscribe();
+    }
+  }
+  
+  posts: Post[] = [];
+  private postSub: Subscription | undefined;
 
   ngOnInit(): void {
     this.posts = this.postService.getPost();
+    this.postSub = this.postService.getPostUpdateListner().subscribe((posts:Post[]) => {
+      this.posts = posts;
+    });
   }
 
-  posts: Post[] = [];
   
 }
