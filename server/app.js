ea93cf9c520374852367865210d0e788ab5a6d68
Rishabh <rishabhgupta7199@gmail.com>
2022-12-10 05:11:13 +0530

    added api for posts data

--- server/app.js
+++ server/app.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.send('Express Welcome');
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {
+            id: 'fihahda',
+            title: 'First server post',
+            content: 'Form Server'
+        },
+        {
+            id: 'dsfs683',
+            title: 'Second server post',
+            content: 'Form Server !!'
+        }
+    ];
+    res.status(200).json({
+        message: 'Posts fetched  successfully',
+        posts: posts
+    });
 });
 
 module.exports = app;
